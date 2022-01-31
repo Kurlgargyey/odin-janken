45be9133d03f5d60f35ebc302062699baf97b2e9
Kurlgargyey <nahsirkivar@gmail.com>
2022-01-31 16:20:29 +0100

    game over and reset functions

--- odin-janken.js
+++ odin-janken.js
@@ -1,3 +1,4 @@
+//generates a random computer play
 function computerPlay () {
  let playIndex = Math.floor(Math.random()*3)
  switch (playIndex) {
@@ -13,6 +14,7 @@ function computerPlay () {
  }
 }
 
+//plays a round
 function playRound (playerPlay, computerPlay) {
   if (/^rock$/i.test(playerPlay)){
    switch (computerPlay) {
@@ -49,48 +51,87 @@ function playRound (playerPlay, computerPlay) {
   }
 }
 
-function game () {
- let wins = 0;
- let losses = 0;
- let round = 0;
+// plays a round on button click and outputs results
+function playButtonRound(e) { 
+  outcome = playRound(e.target.id, computerPlay());
+    
+  if (outcome.match(/tie/)) {
+    round++
+    scoreboard.firstElementChild.textContent = `Scoreboard: Round ${round}`;
+  }
+  else if (outcome.match(/win/)) {
+    wins++
+    playerscore.textContent = `Player: ${wins}`
 
- while (round < 5) {
-  let playerSelection = window.prompt("What's your move?", computerPlay());
-  let computerSelection = computerPlay();
-  let result = playRound(playerSelection, computerSelection);
-  console.log(result)
-  if (result.match(/tie/)) {
-   round++
+    round++
+    scoreboard.firstElementChild.textContent = `Scoreboard: Round ${round}`;
   }
-  else if (result.match(/win/)) {
-   wins++
-   round++
-  }
-  else if (result.match(/lose/)) {
-   losses++
-   round++
-  }
- }
- if (wins > losses) {
-  console.log(`Rounds won: ${wins}. Rounds lost:${losses}. You win!`)
- }
- else if (wins < losses) {
-  console.log(`Rounds won: ${wins}. Rounds lost:${losses}. You lose.`)
- }
- else if (wins === losses) {
-  console.log(`Rounds won: ${wins}. Rounds lost:${losses}. It is a tie.`)
- }
+  else if (outcome.match(/lose/)) {
+    losses++
+    computerscore.textContent = `Computer: ${losses}`
+
+    round++
+    scoreboard.firstElementChild.textContent = `Scoreboard: Round ${round}`;
+  } 
+    
+  const para = document.createElement('p');
+  para.classList.add('content');
+  para.textContent = outcome;
+  results.appendChild(para);
+
 }
+
+function checkGame() {
+  if (wins === 5 || losses === 5) {
+    gameon = false;
+    gameover = document.createElement('p');
+    if (wins > losses) {
+      gameover.textContent = `Game over! You won!`;
+    }else {
+      gameover.textContent = `Game over! You lost.`;
+    }
+    scoreboard.appendChild(gameover);
+    scoreboard.appendChild(resetbtn);
+  }
+}
+
+function resetGame() {
+  wins = 0;
+  losses = 0;
+  round = 0;
+  while(results.childElementCount>1){
+    results.removeChild(results.lastChild);
+  }
+  playerscore.textContent = `Player: ${wins}`
+  computerscore.textContent = `Computer: ${losses}`
+  scoreboard.firstElementChild.textContent = `Scoreboard`;
+  scoreboard.removeChild(gameover);
+  scoreboard.removeChild(resetbtn);
+  gameon = true;
+}
+
+resetbtn = document.createElement('button');
+resetbtn.textContent = 'Reset game';
+resetbtn.addEventListener('click', function(){
+  resetGame()
+});
 
 const results = document.querySelector('#results');
 
-const btns = document.querySelectorAll('button');
+const playerscore = scores.firstElementChild;
+const computerscore = scores.lastElementChild;
+let gameon = true;
+let round = 0;
+let wins = 0;
+let losses = 0;
+
+const btns = document.querySelectorAll('.playerinput');
 btns.forEach((btn) => {
   btn.addEventListener('click', function(e){
-    const para = document.createElement('p');
-    para.classList.add('content');
-    para.textContent = playRound(e.target.id, computerPlay());
-    
-    results.appendChild(para);
+    if (gameon){
+      playButtonRound(e)
+      checkGame();
+    }
   });
 });
+
